5552e0313a65b5655171ed95555c8bddb219be04
Joerg Schneider <schneider.joerg@bcg.com>
2019-08-06 09:03:17 +0200

    prepare code for gamma-common 1.0.0

--- test/test/paths.py
+++ test/test/paths.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 log = logging.getLogger(__name__)
 
@@ -8,5 +7,3 @@ DIR_DATA = "data"
 DIR_CONFIG = "config"
 
 # file paths
-TEST_DATA_CSV = os.path.join(DIR_DATA, "master_table_clean_anon_144.csv")
-TEST_CONFIG_YML = os.path.join(DIR_CONFIG, "test_config.yml")
