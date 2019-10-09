da690a99ff250c7139f65fcafbe676f47559e121
Joerg Schneider <schneider.joerg@bcg.com>
2019-10-09 16:09:06 +0100

    pre-release -> early release

--- src/gamma/common/licensing.py
+++ src/gamma/common/licensing.py
@@ -21,7 +21,7 @@ LICENSEE_ENV = "GAMMA_ALPHA_LICENSEE"
 WARNING_MESSAGE = """
 NOT FOR CLIENT USE!
 
-This is a pre-release library under development. Handling of IP rights is still
+This is a early release library under development. Handling of IP rights is still
 being investigated. To avoid causing any potential IP disputes or issues, DO NOT USE
 ANY OF THIS CODE ON A CLIENT PROJECT, not even in modified form.
 
