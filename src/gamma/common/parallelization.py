9c257dab2671b7f02b1cdb057424f4119e4dd67b
Jan Ittner <ittner.jan@bcg.com>
2019-10-16 19:23:26 +0200

    update ParallelizableMixin constructor signature for (1) easier handling of defaults and (2) new pre_dispatch argument

--- src/gamma/common/parallelization.py
+++ src/gamma/common/parallelization.py
@@ -17,26 +17,44 @@ class ParallelizableMixin:
     Mix-in class indicating the ability to parallelize one or more operations using
     joblib.
 
-    :param n_jobs: number of jobs to _rank_learners in parallel (default: 1).
+    :param n_jobs: number of jobs to use in parallel; \
+        if `None`, use joblib default (default: `None`).
     :param shared_memory: if `True` use threads in the parallel runs. If `False` \
-        use multiprocessing (default: `True`).
-    :param verbose: verbosity level used in the parallel computation (default: 0).
+        use multiprocessing (default: `False`).
+    :param pre_dispatch: number of batches to pre-dispatch; \
+        if `None`, use joblib default (default: `None`).
+    :param verbose: verbosity level used in the parallel computation; \
+        if `None`, use joblib default (default: `None`).
     """
 
     def __init__(
-        self, *, n_jobs: int = 1, shared_memory: bool = True, verbose: int = 0, **kwargs
+        self,
+        *,
+        n_jobs: Optional[int] = None,
+        shared_memory: bool = False,
+        pre_dispatch: Optional[Union[str, int]] = None,
+        verbose: Optional[int] = None,
+        **kwargs,
     ) -> None:
         super().__init__(**kwargs)
         self.n_jobs = n_jobs
         self.shared_memory = shared_memory
+        self.pre_dispatch = pre_dispatch
         self.verbose = verbose
 
+        self._parallel_kwargs = {
+            name: value
+            for name, value in [
+                ("n_jobs", n_jobs),
+                ("require", "sharedmem" if shared_memory else None),
+                ("pre_dispatch", pre_dispatch),
+                ("verbose", verbose),
+            ]
+            if value is not None
+        }
+
     def _parallel(self) -> joblib.Parallel:
-        return joblib.Parallel(
-            n_jobs=self.n_jobs,
-            require="sharedmem" if self.shared_memory else None,
-            verbose=self.verbose,
-        )
+        return joblib.Parallel(**self._parallel_kwargs)
 
     @staticmethod
     def _delayed(
