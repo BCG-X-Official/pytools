91495a7e4aa00aa60f9b0043039526955b09c800
Jan Ittner <ittner.jan@bcg.com>
2019-10-28 10:49:25 +0100

    check for valid license in every package of the Alpha suite

--- src/gamma/viz/__init__.py
+++ src/gamma/viz/__init__.py
@@ -3,6 +3,9 @@ The Gamma visualization library, providing a MVC framework for rendering data in
 different styles, e.g., as Matplotlib charts or as plain text.
 """
 
+import gamma.common.licensing as _licensing
 from ._viz import *
+
+_licensing.check_license(__package__)
 
 __all__ = [member for member in _viz.__all__ if not member.startswith("Base")]
