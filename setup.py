69e4e92e0272dcbad6f9b6f583b0546731930b48
Joerg Schneider <schneider.joerg@bcg.com>
2019-11-25 22:32:41 +0100

    Version updated from 1.0.0 to 1.1.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.0.0",  # Required
+    version="1.1.0",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
@@ -124,7 +124,7 @@ setup(
         "pandas>=0.24,<0.25",
         "numpy>=1.16,<1.17",
         "matplotlib>=3.0.0,<3.1.0",
-        "gamma-common @ git+ssh://git@git.sourceai.io/BCG/gamma-common.git@1.0.0#egg=gamma.common",
+        "gamma-common @ git+ssh://git@git.sourceai.io/BCG/gamma-common.git@1.1.0#egg=gamma.common",
     ],
     dependency_links=[],
     # Optional
