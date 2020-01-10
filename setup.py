ffcc9ab3dc774daf142d47920782879034ef60b4
Joerg Schneider <schneider.joerg@bcg.com>
2020-01-10 12:21:31 +0100

    add licensing package

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
     #
     # packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
     package_dir={"": "src"},
-    packages=["gamma.common"],
+    packages=["gamma.common", "gamma.common.licensing"],
     namespace_packages=["gamma"],
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
