b6fb7239c339607135af2dbb496a23f6668675f1
Joerg Schneider <schneider.joerg@bcg.com>
2020-01-10 13:05:34 +0100

    changes to imports to correctly work with python 3.6

--- src/gamma/common/__init__.py
+++ src/gamma/common/__init__.py
@@ -1,7 +1,10 @@
 """
 Global definitions of basic types and functions for use across all gamma libraries
 """
-import gamma.common.licensing as _licensing
+from . import licensing
+from . import fit
+from . import parallelization
+from . import typing
 from ._common import *
 
-_licensing.check_license(__package__)
+licensing.check_license(__package__)
