5f62649e2b0d678c8ec84c1f80c3fd3104a6332d
Joerg Schneider <schneider.joerg@bcg.com>
2020-01-17 13:16:05 +0100

    add ._common import *

--- src/gamma/common/__init__.py
+++ src/gamma/common/__init__.py
@@ -2,5 +2,6 @@
 Global definitions of basic types and functions for use across all gamma libraries
 """
 from . import licensing as _licensing
+from ._common import *
 
 _licensing.check_license(__package__)
