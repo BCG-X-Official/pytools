9c17dd6923ef95ff628598befbc7bc2a5bbfef4c
Jason Bentley <Bentley.Jason@bcg.com>
2020-08-14 12:21:52 +0100

    added __all__ statement

--- src/pytools/viz/__init__.py
+++ src/pytools/viz/__init__.py
@@ -6,3 +6,15 @@ as Matplotlib charts or as plain text.
 from ._matplot import *
 from ._text import *
 from ._viz import *
+
+__all__ = [
+    "ColorbarMatplotStyle",
+    "MatplotStyle",
+    "RGBA_BLACK",
+    "RGBA_WHITE",
+    "RgbaColor",
+    "PercentageFormatter",
+    "TextStyle",
+    "Drawer",
+    "DrawStyle",
+]
