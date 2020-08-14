b862c1ca673a433b2df8d4ceea5ba3a1d9bbd896
Jason Bentley <Bentley.Jason@bcg.com>
2020-08-14 12:22:01 +0100

    added __all__ statement

--- src/pytools/viz/matrix/__init__.py
+++ src/pytools/viz/matrix/__init__.py
@@ -3,3 +3,11 @@ Plotting matrices for exploratory data visualization.
 """
 
 from ._matrix import *
+
+__all__ = [
+    "MatrixDrawer",
+    "MatrixMatplotStyle",
+    "MatrixReportStyle",
+    "MatrixStyle",
+    "PercentageMatrixMatplotStyle",
+]
