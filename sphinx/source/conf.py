f60503dc02247a3e3b41af09187bb71d19533a41
mgelsm <grisard.malo@bcg.com>
2020-10-08 16:10:31 +0200

    Fix path to base scripts for sphinx build (#48)

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -7,7 +7,8 @@ Receives majority of configuration from pytools conf_base.py
 import os
 import sys
 
-sys.path.append(
+sys.path.insert(
+    0,
     os.path.abspath(
         os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
@@ -16,12 +17,12 @@ sys.path.append(
             os.pardir,
             "pytools",
             "sphinx",
-            "source",
+            "base",
         )
-    )
+    ),
 )
 
-from sphinx.base.conf_base import set_config
+from conf_base import set_config
 
 # ----- custom configuration -----
 
