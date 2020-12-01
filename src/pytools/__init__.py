774907afa3e0d1efa80bd9ce51b0156452c21d03
Jason Bentley <Bentley.Jason@bcg.com>
2020-12-01 10:18:13 +0000

    capitalise GAMMA

--- src/pytools/__init__.py
+++ src/pytools/__init__.py
@@ -1,5 +1,5 @@
 """
-A collection of generic Python extensions and tools, used across Gamma's open-source
+A collection of generic Python extensions and tools, used across BCG GAMMA's open-source
 libraries.
 """
 __version__ = "1.0.0"
