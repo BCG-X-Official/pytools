4e269a6ad74f93b950d56df6479582c29cae19b4
Azure Pipelines <azuredevops@microsoft.com>
2021-01-29 10:35:45 +0000

    Publish GitHub Pages [skip ci]

--- docs/docs-version/1-0-1/_static/js/versions.js
+++ docs/docs-version/1-0-1/_static/js/versions.js
@@ -1,11 +1,9 @@
 const DOCS_VERSIONS = {
-    "current": "1.1.0",
+    "current": "1.0.1",
     "non_rc": [
-        "1.1.0",
         "1.0.1"
     ],
     "all": [
-        "1.1.0",
         "1.0.1"
     ]
 }
