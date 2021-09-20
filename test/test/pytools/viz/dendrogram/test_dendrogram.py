3d6edc769fb814a6449b8d3722b4045aa990f729
Jan Ittner <github.com@ittner.eu>
2021-09-20 10:42:09 +0200

    TEST: assert explicit dendrogram text output, not a hash

--- test/test/pytools/viz/dendrogram/test_dendrogram.py
+++ test/test/pytools/viz/dendrogram/test_dendrogram.py
@@ -2,17 +2,11 @@
 Tests for package pytools.viz.dendrogram
 """
 
-# noinspection PyPackageRequirements
-import hashlib
 import logging
 from io import StringIO
 
 import numpy as np
-
-# noinspection PyPackageRequirements
 import pytest
-
-# noinspection PyPackageRequirements
 import scipy.cluster.hierarchy as hc
 
 from pytools.viz.dendrogram import DendrogramDrawer, DendrogramReportStyle, LinkageTree
@@ -38,7 +32,6 @@ def linkage_tree(linkage_matrix: np.ndarray) -> LinkageTree:
 
 
 def test_dendrogram_drawer_text(linkage_matrix: np.ndarray) -> None:
-    checksum_dendrogram_report = "32427095857f0589f68210ad4b2e8210"
     leaf_names = list("ABCDEFGH")
     leaf_weights = [(w + 1) / 36 for w in range(8)]
 
@@ -68,6 +61,20 @@ def test_dendrogram_drawer_text(linkage_matrix: np.ndarray) -> None:
         dd.draw(data=linkage_tree, title="Test")
         report_str = str(out.getvalue())
         log.debug(f"\n{report_str}")
-        assert (
-            hashlib.md5(str(report_str).encode("utf-8")).hexdigest()
-        ) == checksum_dendrogram_report
+
+    report_str_expected = (
+        r"""
+===================================== Test =====================================
+
+G               19% \_____________
+F               17% /             \--------------------------------------------\
+B                6% --------------/                                            |
+H               22% \_____________                                             |
+C                8% /             \______________                              |
+E               14% --------------/              \-----------------------------/
+A                3% --------------/              |
+D               11% -----------------------------/
+
+"""
+    )[1:]
+    assert report_str == report_str_expected
