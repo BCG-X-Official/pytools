7d7c6f47d40ce2198a36db004e1739d277153185
Jan Ittner <github.com@ittner.eu>
2021-09-21 13:25:13 +0200

    TEST: add unit tests for class Matrix

---
+++ test/test/pytools/test_data.py
@@ -0,0 +1,133 @@
+"""
+Basic test cases for the `pytools.data` module
+"""
+
+
+import numpy as np
+import pytest
+
+from pytools.data import Matrix
+
+
+def test_matrix_validatio() -> None:
+    with pytest.raises(ValueError, match="got a 3d array"):
+        Matrix(np.arange(20).reshape((4, 5, 1)))
+
+    with pytest.raises(TypeError, match="got a str"):
+        # noinspection PyTypeChecker
+        Matrix(np.arange(20).reshape((4, 5)), names="invalid")
+
+    with pytest.raises(ValueError, match="got a 3-tuple"):
+        # noinspection PyTypeChecker
+        Matrix(np.arange(20).reshape((4, 5)), names=("invalid", "invalid", "invalid"))
+
+    with pytest.raises(
+        ValueError,
+        match=(
+            r"arg names\[0\] must have same length as arg data.shape\[0\]=4, "
+            "but has length 1"
+        ),
+    ):
+        Matrix(np.arange(20).reshape((4, 5)), names=("invalid", "invalid"))
+
+    with pytest.raises(ValueError, match="got a 3-tuple"):
+        # noinspection PyTypeChecker
+        Matrix(np.arange(20).reshape((4, 5)), weights=(1, 2, 3))
+
+    with pytest.raises(
+        ValueError, match=r"arg weights\[0\] must be a 1d array, but has shape \(\)"
+    ):
+        # noinspection PyTypeChecker
+        Matrix(np.arange(20).reshape((4, 5)), weights=(1, [2, 4]))
+
+    with pytest.raises(
+        ValueError,
+        match=(
+            r"arg weights\[1\] must have same length as arg data.shape\[1\]=5, "
+            r"but has length 2"
+        ),
+    ):
+        # noinspection PyTypeChecker
+        Matrix(np.arange(20).reshape((4, 5)), weights=(None, [2, 4]))
+
+    with pytest.raises(
+        ValueError,
+        match="got a 3-tuple",
+    ):
+        # noinspection PyTypeChecker
+        Matrix(np.arange(20).reshape((4, 5)), name_labels=(1, 2, 3))
+
+
+def test_matrix() -> None:
+
+    m: Matrix = Matrix(
+        np.arange(20).reshape((4, 5)),
+        names=(list("ABCD"), list("abcde")),
+        weights=([2, 4, 2, 4], [1, 5, 4, 1, 5]),
+        name_labels=("row", "column"),
+        weight_label="weight",
+    )
+
+    assert m.resize(1, None) == Matrix(
+        np.array([[5, 6, 7, 8, 9]]),
+        names=(["B"], list("abcde")),
+        weights=([4], [1, 5, 4, 1, 5]),
+        name_labels=("row", "column"),
+        weight_label="weight",
+    )
+
+    assert m.resize(None, 1) == Matrix(
+        np.array([[1], [6], [11], [16]]),
+        names=(list("ABCD"), ["b"]),
+        weights=([2, 4, 2, 4], [5]),
+        name_labels=("row", "column"),
+        weight_label="weight",
+    )
+
+    assert m.resize(1, 1) == Matrix(
+        np.array([[6]]),
+        names=(["B"], ["b"]),
+        weights=([4], [5]),
+        name_labels=("row", "column"),
+        weight_label="weight",
+    )
+
+    assert m.resize(3, 4) == Matrix(
+        np.array([[0, 1, 2, 4], [5, 6, 7, 9], [15, 16, 17, 19]]),
+        names=(list("ABD"), list("abce")),
+        weights=([2, 4, 4], [1, 5, 4, 5]),
+        name_labels=("row", "column"),
+        weight_label="weight",
+    )
+
+    assert m.resize(0.8, 0.0001) == Matrix(
+        data=np.array([[1], [6], [16]]),
+        names=(list("ABD"), ["b"]),
+        weights=([2, 4, 4], [5]),
+        name_labels=("row", "column"),
+        weight_label="weight",
+    )
+
+    assert m.resize(4, 5) == m
+
+    assert m.resize(3, 3) != m
+
+    with pytest.raises(
+        ValueError,
+        match="arg rows=5 must not be greater than the current number of rows",
+    ):
+        m.resize(5, 5)
+
+    with pytest.raises(
+        ValueError,
+        match="arg columns=6 must not be greater than the current number of rows",
+    ):
+        m.resize(4, 6)
+
+    with pytest.raises(ValueError, match="arg rows=-4 must not be negative"):
+        m.resize(-4, 5)
+
+    with pytest.raises(
+        ValueError, match="arg columns=1.5 must not be greater than 1.0"
+    ):
+        m.resize(None, 1.5)
