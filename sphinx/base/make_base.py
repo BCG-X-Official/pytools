39307b639ff34c58b33a9be0b5283deafa0ad442
Jan Ittner <github.com@ittner.eu>
2022-06-30 19:00:04 +0200

    DOC: publish docs only for latest patch of each minor version

--- sphinx/base/make_base.py
+++ sphinx/base/make_base.py
@@ -11,15 +11,18 @@ import shutil
 import subprocess
 import sys
 from abc import ABCMeta, abstractmethod
+from collections import defaultdict
 from glob import glob
-from tempfile import TemporaryDirectory
 from typing import Any, Dict, Iterable, List, Optional, Set, Tuple
 from weakref import ref
 
 from make_util import get_package_version as _get_package_version
 from packaging import version as pkg_version
 
-DIR_SPHINX_ROOT = os.getcwd()
+DIR_SPHINX_ROOT = os.path.dirname(os.path.dirname(__file__))
+assert (
+    os.path.basename(DIR_SPHINX_ROOT) == "sphinx"
+), f"Sphinx root dir {DIR_SPHINX_ROOT!r} is named 'sphinx'"
 
 # Sphinx commands
 CMD_SPHINX_BUILD = "sphinx-build"
@@ -27,20 +30,21 @@ CMD_SPHINX_AUTOGEN = "sphinx-autogen"
 
 # File paths
 DIR_SPHINX_BASE = os.path.dirname(os.path.realpath(__file__))
-DIR_REPO_ROOT = os.path.realpath(os.path.join(DIR_SPHINX_ROOT, os.pardir))
-DIR_REPO_PARENT = os.path.realpath(os.path.join(DIR_REPO_ROOT, os.pardir))
+DIR_REPO_ROOT = os.path.dirname(DIR_SPHINX_ROOT)
+DIR_REPO_PARENT = os.path.dirname(DIR_REPO_ROOT)
 PROJECT_NAME = os.path.split(os.path.realpath(DIR_REPO_ROOT))[1]
 DIR_PACKAGE_SRC = os.path.join(DIR_REPO_ROOT, "src")
 DIR_DOCS = os.path.join(DIR_REPO_ROOT, "docs")
+DIR_DOCS_VERSION = os.path.join(DIR_DOCS, "docs-version")
 DIR_SPHINX_SOURCE = os.path.join(DIR_SPHINX_ROOT, "source")
 DIR_SPHINX_AUX = os.path.join(DIR_SPHINX_ROOT, "auxiliary")
 DIR_SPHINX_API_GENERATED = os.path.join(DIR_SPHINX_SOURCE, "apidoc")
 DIR_SPHINX_GENERATED = os.path.join(DIR_SPHINX_SOURCE, "_generated")
 DIR_SPHINX_BUILD = os.path.join(DIR_SPHINX_ROOT, "build")
+DIR_SPHINX_BUILD_DOCS_VERSION = os.path.join(DIR_SPHINX_BUILD, "docs-version")
 DIR_SPHINX_BUILD_HTML = os.path.join(DIR_SPHINX_BUILD, "html")
 DIR_SPHINX_TEMPLATES = os.path.join(DIR_SPHINX_ROOT, "base", "_templates")
 DIR_SPHINX_TUTORIAL = os.path.join(DIR_SPHINX_SOURCE, "tutorial")
-DIR_ALL_DOCS_VERSIONS = os.path.join(DIR_SPHINX_BUILD, "docs-version")
 DIR_PACKAGE_ROOT = os.path.abspath(os.path.join(DIR_REPO_ROOT, "src", PROJECT_NAME))
 
 FILE_AUTOSUMMARY_TEMPLATE = os.path.join(DIR_SPHINX_GENERATED, "autosummary.rst_")
@@ -90,6 +94,9 @@ class Command(metaclass=CommandMeta):
     def __init__(self) -> None:
         self.name = self.__RE_CAMEL_TO_SNAKE.sub("_", type(self).__name__).lower()
 
+        # get the current version of the package we're building documentation for
+        self.package_version: pkg_version.Version = get_package_version()
+
     @abstractmethod
     def get_description(self) -> str:
         pass
@@ -117,7 +124,7 @@ class Command(metaclass=CommandMeta):
         return dependencies_extended
 
     def run(self) -> None:
-        log(f"Running command {self.name} – {self.get_description()}")
+        log(f"{'=' * 80}\n" f"Running command {self.name} – {self.get_description()}\n")
         self._run()
 
     @abstractmethod
@@ -244,11 +251,10 @@ class FetchPkgVersions(Command):
         return ()
 
     def _run(self) -> None:
-        versions = Versions()
+        versions = get_versions()
 
         version_data = {
-            "current": str(versions.latest_stable_version),
-            "non_rc": list(map(str, versions.version_tags_stable)),
+            "current": str(versions.latest_version),
             "all": list(map(str, versions.version_tags)),
         }
 
@@ -264,117 +270,80 @@ class FetchPkgVersions(Command):
 
 class PrepareDocsDeployment(Command):
     def get_description(self) -> str:
-        return "update versions of rendered documentation"
+        return "integrate documentation of previous versions"
 
     def get_dependencies(self) -> Tuple[Command, ...]:
         return ()
 
     def _run(self) -> None:
+        # we expect to find all previous documentation in docs/
+        # and the newly built documentation in sphinx/build/
         if not is_azure_build():
             raise RuntimeError("only implemented for Azure Pipelines")
 
-        # get the current version of the package
-        current_version: pkg_version.Version = get_package_version()
-
-        # copy new the docs version to the deployment path
-        if current_version == Versions().latest_stable_version:
-            self._copy_latest_docs()
-        else:
-            self._update_versions_js()
-
-        self._copy_historic_docs(current_version)
+        # copy the new docs version to the deployment path
+        self._copy_new_documentation()
 
         self._update_historic_docs()
 
-    @staticmethod
-    def _copy_latest_docs() -> None:
-        # only copy to deployment path if our version is the latest stable release
-        log("Build is latest stable release – updating root docs.")
+        self._copy_latest_to_docs_root()
 
-        # remove docs build currently deployed, except for the docs versions folder
-        if os.path.exists(os.path.join(DIR_DOCS, "docs-version")):
-            with TemporaryDirectory() as DIR_TMP:
-                shutil.move(src=os.path.join(DIR_DOCS, "docs-version"), dst=DIR_TMP)
-                shutil.rmtree(path=DIR_DOCS)
-                os.makedirs(DIR_DOCS)
-                shutil.move(src=os.path.join(DIR_TMP, "docs-version"), dst=DIR_DOCS)
-        else:
-            os.makedirs(DIR_DOCS, exist_ok=True)
+        self._tidy_up_docs_root()
+
+    def _copy_new_documentation(self) -> None:
+        log("Adding new documentation to documentation version history")
+
+        os.makedirs(DIR_DOCS_VERSION, exist_ok=True)
+
+        dir_docs_current_version = os.path.join(
+            DIR_DOCS_VERSION,
+            version_string_to_url(self.package_version),
+        )
+
+        if os.path.exists(dir_docs_current_version):
+            # remove a previous version of the same documentation if it exists
+            shutil.rmtree(dir_docs_current_version)
 
         # copy new docs version to deployment path
-        shutil.copytree(src=DIR_SPHINX_BUILD_HTML, dst=DIR_DOCS, dirs_exist_ok=True)
+        shutil.copytree(src=DIR_SPHINX_BUILD_HTML, dst=dir_docs_current_version)
 
-    @staticmethod
-    def _update_versions_js() -> None:
-        # build of a pre-release or patch to an earlier release
-        log("Build is not latest stable release – just updating versions.")
-        # – only update "versions.js":
-        new_versions_js = os.path.join(DIR_SPHINX_BUILD_HTML, FILE_JS_VERSIONS_RELATIVE)
-        if not os.path.exists(new_versions_js):
-            raise FileNotFoundError(f"No versions.js file at: {new_versions_js}")
-
-        versions_js_out = os.path.join(DIR_DOCS, FILE_JS_VERSIONS_RELATIVE)
-
-        os.makedirs(os.path.dirname(versions_js_out), exist_ok=True)
-
-        log(
-            "Copying updated versions.js file from "
-            f"'{new_versions_js}' to '{versions_js_out}'"
-        )
-
-        shutil.copy(src=new_versions_js, dst=versions_js_out)
-
-    @staticmethod
-    def _copy_historic_docs(current_version: pkg_version.Version) -> None:
-        # get current version of package in the form of folder/URL name (e.g., "1-0-0")
-        current_version_path = os.path.join(
-            DIR_DOCS, "docs-version", version_string_to_url(current_version)
-        )
-        # update latest version in docs history
-        if os.path.exists(current_version_path):
-            shutil.rmtree(path=current_version_path)
-
-        log(f"Copying pre-existing docs from: '{DIR_ALL_DOCS_VERSIONS}'")
-
-        if os.path.exists(DIR_ALL_DOCS_VERSIONS) and os.path.isdir(
-            DIR_ALL_DOCS_VERSIONS
-        ):
-            log("Pre-existing docs contents:")
-            log(os.listdir(DIR_ALL_DOCS_VERSIONS))
-
-        else:
-            raise FileNotFoundError(
-                f"'{DIR_ALL_DOCS_VERSIONS}' is missing or not a dir"
-            )
-
-        shutil.copytree(
-            src=DIR_ALL_DOCS_VERSIONS,
-            dst=os.path.join(DIR_DOCS, "docs-version"),
-            dirs_exist_ok=True,
-        )
-
-    @staticmethod
-    def _update_historic_docs() -> None:
+    def _update_historic_docs(self) -> None:
         # Replace all docs version lists with the most up-to-date to have all versions
         # accessible also from older versions
-        new_versions_js = os.path.join(DIR_DOCS, FILE_JS_VERSIONS_RELATIVE)
-        for d in glob(os.path.join(DIR_DOCS, "docs-version", "*", "")):
-            old_versions_js = os.path.join(d, FILE_JS_VERSIONS_RELATIVE)
-            log(
-                "Copying versions.js file from "
-                f"'{new_versions_js}' to '{old_versions_js}'"
-            )
-            shutil.copyfile(src=new_versions_js, dst=old_versions_js)
+        new_versions_js = os.path.join(
+            DIR_DOCS_VERSION,
+            version_string_to_url(self.package_version),
+            FILE_JS_VERSIONS_RELATIVE,
+        )
 
+        for d in glob(os.path.join(DIR_DOCS_VERSION, "*")):
+            old_versions_js = os.path.join(d, FILE_JS_VERSIONS_RELATIVE)
+            if old_versions_js != new_versions_js:
+                log(
+                    "Copying versions.js file from "
+                    f"'{new_versions_js}' to '{old_versions_js}'"
+                )
+                shutil.copyfile(src=new_versions_js, dst=old_versions_js)
+
+    @staticmethod
+    def _copy_latest_to_docs_root() -> None:
+        # copy latest version to root
+
+        dir_latest_version = os.path.join(
+            DIR_DOCS_VERSION, version_string_to_url(get_versions().latest_version)
+        )
+        shutil.copytree(src=dir_latest_version, dst=DIR_DOCS, dirs_exist_ok=True)
+
+    @staticmethod
+    def _tidy_up_docs_root() -> None:
         # remove .buildinfo which interferes with GitHub Pages build
-        if os.path.exists(os.path.join(DIR_DOCS, ".buildinfo")):
-            os.remove(os.path.join(DIR_DOCS, ".buildinfo"))
+        file_build_info = os.path.join(DIR_DOCS, ".buildinfo")
+        if os.path.exists(file_build_info):
+            os.remove(file_build_info)
 
         # create empty file to signal that no GitHub auto-rendering is required
         # noinspection SpellCheckingInspection
         open(os.path.join(DIR_DOCS, ".nojekyll"), "a").close()
-
-        log("Docs moved to ./docs and historic versions updated")
 
 
 class Html(Command):
@@ -402,31 +371,6 @@ class Html(Command):
             check=True,
         )
 
-        # create interactive versions of all notebooks
-        sys.path.append(DIR_SPHINX_BASE)
-
-        # create copy of this build for the docs archive
-        version_built: pkg_version.Version = get_package_version()
-        dir_path_this_build = os.path.join(
-            DIR_ALL_DOCS_VERSIONS, version_string_to_url(version_built)
-        )
-
-        os.makedirs(DIR_ALL_DOCS_VERSIONS, exist_ok=True)
-        if os.path.exists(dir_path_this_build):
-            shutil.rmtree(dir_path_this_build)
-
-        shutil.copytree(
-            src=DIR_SPHINX_BUILD_HTML,
-            dst=dir_path_this_build,
-        )
-
-        if not is_azure_build():
-            shutil.move(src=DIR_ALL_DOCS_VERSIONS, dst=DIR_SPHINX_BUILD_HTML)
-
-        # empty versions file to blank template
-        with open(FILE_JS_VERSIONS, "wt") as f:
-            f.write("")
-
 
 class Help(Command):
     def get_description(self) -> str:
@@ -444,43 +388,59 @@ class Versions:
     Helper class that lists all versions that have already been released.
     """
 
-    INITIAL_VERSION_TAG = pkg_version.parse("1.0.1")
+    def __init__(self, version_tags: Iterable[pkg_version.Version]) -> None:
+        self.version_tags = sorted(version_tags, reverse=True)
 
-    def __init__(self) -> None:
-        os.makedirs(DIR_SPHINX_BUILD, exist_ok=True)
-        start_from_version_tag: pkg_version.Version = Versions.INITIAL_VERSION_TAG
-        sp = subprocess.run(
-            args='git tag -l "*.*.*"', shell=True, check=True, stdout=subprocess.PIPE
-        )
-        version_tags: List[pkg_version.Version] = [
-            version_tag
-            for version_tag in (
-                pkg_version.parse(version_string)
-                for version_string in sp.stdout.decode("UTF-8").split("\n")
-                if version_string
+    @property
+    def latest_version(self) -> pkg_version:
+        return self.version_tags[0]
+
+
+_versions: Optional[Versions] = None
+
+
+def get_versions() -> Versions:
+    global _versions
+
+    if _versions is not None:
+        return _versions
+
+    os.makedirs(DIR_SPHINX_BUILD, exist_ok=True)
+    start_from_version_tag: pkg_version.Version = pkg_version.Version("1.0")
+
+    version_tags: Iterable[pkg_version.Version] = (
+        pkg_version.parse(s)
+        for s in (
+            subprocess.run(
+                args='git tag -l "*.*.*"',
+                shell=True,
+                check=True,
+                stdout=subprocess.PIPE,
             )
-            if version_tag >= start_from_version_tag
-        ]
+            .stdout.decode("UTF-8")
+            .split("\n")
+        )
+        if s
+    )
 
-        # append the version we are building to version_tags
-        version_built: pkg_version.Version = get_package_version()
+    # append the version we are building to version_tags
+    version_built: pkg_version.Version = get_package_version()
+    version_tags = (*version_tags, version_built)
 
-        if version_built not in version_tags:
-            version_tags.append(version_built)
+    versions_by_minor_version: Dict[str, List[pkg_version.Version]] = defaultdict(list)
 
-        version_tags.sort()
-        version_tags.reverse()
-        version_tags_stable: List[pkg_version.Version] = [
-            vt for vt in version_tags if not (vt.is_prerelease or vt.is_devrelease)
-        ]
-        latest_stable_version: pkg_version.Version = version_tags_stable[0]
+    for v in version_tags:
+        if not (v.is_prerelease or v.is_devrelease) and v >= start_from_version_tag:
+            versions_by_minor_version[f"{v.major}.{v.minor}"].append(v)
 
-        log(f"Found versions: {', '.join(map(str, version_tags))}")
-        log(f"Latest stable version: {latest_stable_version}")
+    minor_versions: List[pkg_version] = [
+        max(versions) for versions in versions_by_minor_version.values()
+    ]
 
-        self.version_tags = version_tags
-        self.version_tags_stable = version_tags_stable
-        self.latest_stable_version = latest_stable_version
+    log(f"Found minor versions: {', '.join(map(str, minor_versions))}")
+
+    _versions = Versions(minor_versions)
+    return _versions
 
 
 def make() -> None:
@@ -545,7 +505,12 @@ def is_azure_build() -> bool:
     """
     Check if this is an Azure DevOps pipelines build
     """
-    return "BUILD_REASON" in os.environ
+    is_azure = "BUILD_REASON" in os.environ
+
+    if is_azure:
+        log("Azure build detected")
+
+    return is_azure
 
 
 def get_package_version() -> pkg_version.Version:
@@ -561,7 +526,7 @@ def version_string_to_url(version: pkg_version.Version) -> str:
 
     Our convention is to only replace all dots with dashes.
     """
-    return str(version).replace(".", "-")
+    return f"{version.major}-{version.minor}"
 
 
 def check_sphinx_version() -> None:
