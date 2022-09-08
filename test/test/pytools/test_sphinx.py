f2c8a793623ccfb10719a32eb94c7703c6cf8c0b
j-ittner <ittner.jan@bcg.com>
2022-09-08 20:23:43 +0200

    FIX: eliminate warnings in test_sphinx.py

--- test/test/pytools/test_sphinx.py
+++ test/test/pytools/test_sphinx.py
@@ -33,44 +33,47 @@ class C(B[str, T]):
     pass
 
 
+# noinspection PyUnresolvedReferences
 def test_resolve_generic_class_parameters() -> None:
     from pytools.sphinx.util import ResolveTypeVariables
 
+    sphinx = type("Sphinx", (object,), {})()
+
     resolve_type_variables = ResolveTypeVariables()
 
-    resolve_type_variables.process(app=None, obj=Drawer, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=Drawer, bound_method=False)
 
-    resolve_type_variables.process(app=None, obj=ECDFDrawer, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=ECDFDrawer, bound_method=False)
 
     resolve_type_variables.process(
-        app=None, obj=ECDFDrawer.get_named_styles, bound_method=True
+        app=sphinx, obj=ECDFDrawer.get_named_styles, bound_method=True
     )
-    # noinspection PyUnresolvedReferences
+
     assert ECDFDrawer.get_named_styles.__annotations__ == {
         "return": Dict[str, Callable[..., ECDFStyle]]
     }
 
-    resolve_type_variables.process(app=None, obj=A, bound_method=False)
-    resolve_type_variables.process(app=None, obj=A.f, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=A, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=A.f, bound_method=False)
     assert A.f.__annotations__ == {"self": A, "x": Type[T], "return": U}
-    resolve_type_variables.process(app=None, obj=A.g, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=A.g, bound_method=False)
     assert A.g.__annotations__ == {"self": A, "return": Optional[A]}
 
-    resolve_type_variables.process(app=None, obj=A.h, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=A.h, bound_method=False)
     assert A.h.__annotations__ == {"cls": Type[A], "return": A}
 
-    resolve_type_variables.process(app=None, obj=B, bound_method=False)
-    resolve_type_variables.process(app=None, obj=B.f, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=B, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=B.f, bound_method=False)
     assert B.f is A.f
     assert A.f.__annotations__ == {"self": B, "x": Type[U], "return": int}
-    resolve_type_variables.process(app=None, obj=B.g, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=B.g, bound_method=False)
     assert B.g is A.g
     assert A.g.__annotations__ == {"self": B, "return": Optional[B]}
-    resolve_type_variables.process(app=None, obj=B.h, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=B.h, bound_method=False)
     assert B.h is not A.h
     assert B.h.__annotations__ == {"cls": Type[B], "return": B}
 
-    resolve_type_variables.process(app=None, obj=C, bound_method=False)
-    resolve_type_variables.process(app=None, obj=C.f, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=C, bound_method=False)
+    resolve_type_variables.process(app=sphinx, obj=C.f, bound_method=False)
     assert C.f is A.f
     assert A.f.__annotations__ == {"self": C, "x": Type[str], "return": int}
