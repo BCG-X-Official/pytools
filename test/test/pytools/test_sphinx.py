3f62e70418d114cd891bd600a69ee99dda86b727
Jan Ittner <ittner.jan@bcg.com>
2024-06-10 23:08:26 +0200

    API: build initial Pytools 3.0 release, part 2 (#368)
    * DOC: narrow down type hint, hoping Sphinx can handle it now
    
    * API: new class method Drawer.get_default_style()
    
    * API: add Drawer.get_style(); return names from .get_named_styles()
    
    * REFACTOR: replace _repr_mimebundle_ with simple _repr_… functions
    
    * FIX: insert class name in docstring, not string representation
    
    * API: add new style class HTMLStyle
    
    * BUILD: ignore IPython for mypy checks

--- test/test/pytools/test_sphinx.py
+++ test/test/pytools/test_sphinx.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Callable, Dict, Generic, Optional, Type, TypeVar
+from typing import Any, Generic, Iterable, Optional, Type, TypeVar
 
 from pytools.viz import Drawer
 from pytools.viz.distribution import ECDFDrawer
@@ -63,8 +63,8 @@ def test_resolve_generic_class_parameters() -> None:
         app=sphinx, obj=ECDFDrawer.get_named_styles, bound_method=True
     )
 
-    assert ECDFDrawer.get_named_styles.__annotations__ == {
-        "return": Dict[str, Callable[..., ECDFStyle]]
+    assert ECDFDrawer.get_style_classes.__annotations__ == {
+        "return": Iterable[Type[ECDFStyle]]
     }
 
     _set_current_class(A)
