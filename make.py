5d1a46f563c255399d4d3d54d8e809d5b8131624
j-ittner <ittner.jan@bcg.com>
2025-07-21 23:14:14 +0200

    BUILD: update build command in make.py to include additional channels

--- make.py
+++ make.py
@@ -466,7 +466,7 @@ class CondaBuilder(Builder):
         )
 
         os.makedirs(build_path, exist_ok=True)
-        build_cmd = f"rattler-build build --recipe {recipe_path}"
+        build_cmd = f"rattler-build build -c bcgx -c conda-forge --recipe {recipe_path}"
         log(
             f"Building: {self.project}\n"
             f"Build path: {build_path}\n"
